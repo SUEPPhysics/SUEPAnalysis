568a3ad5f6410ec109035dade7b3135fc5448818
Yacine <yhaddad@cern.ch>
2019-09-02 09:33:02 +0200

    minor changes

--- python/BtagEventWeightProducer.py
+++ python/BtagEventWeightProducer.py
@@ -42,49 +42,49 @@ class BtagEventWeightProducer(Module):
 	weight_down = 1.0
 	jets = list(Collection(event, "Jet"))
         for i,j in enumerate(jets):
-        	flavor =  j.hadronFlavour
-		SF = j.btagSF # lets see if this works
-		SFup = j.btagSF_up
-                SFdown = j.btagSF_down	
-		if flavor == 5: #this is for the actual bottom events
-                	hist = self.loadHisto(self.targetfile,"bottom_eff")
-	        elif flavor == 4: #this is for the charm events
-                        hist = self.loadHisto(self.targetfile,"charm_eff")
-		elif flavor == 0: #this is for the light flavored jets
-                        hist = self.loadHisto(self.targetfile,"light_eff")
-		else:
-			print "The jet flavor does not make sense!!!!!!"
-			continue
-		#Now for Bin loops
-	        nxBins = 5
-	        nyBins = 12
-	        searchbinx = -1
-	        searchbiny = -1
-	        for xbin in range(1,nxBins+1):
-	                if j.pt > hist.GetXaxis().GetBinLowEdge(nxBins) + hist.GetXaxis().GetBinWidth(nxBins):
-	                        searchbinx = 12
-	                        break
-	                if j.pt > hist.GetXaxis().GetBinLowEdge(xbin) and j.pt < hist.GetXaxis().GetBinLowEdge(xbin) + hist.GetXaxis().GetBinWidth(xbin) :
-	                        searchbinx = xbin
-	        for ybin in range(1,nyBins+1):
-	                if j.eta > hist.GetYaxis().GetBinLowEdge(nyBins) + hist.GetYaxis().GetBinWidth(nyBins):
-	                        searchbiny = 5
-	                        break
-	                if j.eta > hist.GetYaxis().GetBinLowEdge(ybin) and j.eta < hist.GetYaxis().GetBinLowEdge(ybin) + hist.GetYaxis().GetBinWidth(ybin) :
-	                        searchbiny = ybin
+            flavor =  j.hadronFlavour
+            SF = j.btagSF # lets see if this works
+            SFup = j.btagSF_up
+            SFdown = j.btagSF_down	
+            if flavor == 5: #this is for the actual bottom events
+                hist = self.loadHisto(self.targetfile,"bottom_eff")
+            elif flavor == 4: #this is for the charm events
+                hist = self.loadHisto(self.targetfile,"charm_eff")
+            elif flavor == 0: #this is for the light flavored jets
+                hist = self.loadHisto(self.targetfile,"light_eff")
+            else:
+                print "The jet flavor does not make sense!!!!!!"
+                continue
+            #Now for Bin loops
+            nxBins = 5
+            nyBins = 12
+            searchbinx = -1
+            searchbiny = -1
+            for xbin in range(1,nxBins+1):
+                if j.pt > hist.GetXaxis().GetBinLowEdge(nxBins) + hist.GetXaxis().GetBinWidth(nxBins):
+                    searchbinx = 12
+                    break
+                if j.pt > hist.GetXaxis().GetBinLowEdge(xbin) and j.pt < hist.GetXaxis().GetBinLowEdge(xbin) + hist.GetXaxis().GetBinWidth(xbin) :
+                    searchbinx = xbin
+            for ybin in range(1,nyBins+1):
+                if j.eta > hist.GetYaxis().GetBinLowEdge(nyBins) + hist.GetYaxis().GetBinWidth(nyBins):
+                    searchbiny = 5
+                    break
+                if j.eta > hist.GetYaxis().GetBinLowEdge(ybin) and j.eta < hist.GetYaxis().GetBinLowEdge(ybin) + hist.GetYaxis().GetBinWidth(ybin) :
+                    searchbiny = ybin
 	
-	        eff = hist.GetBinContent(searchbinx,searchbiny)                
-		err = np.sqrt(eff*(1-eff)/5000.)
-		test = hist.GetBinError(searchbinx,searchbiny)
+            eff = hist.GetBinContent(searchbinx,searchbiny)                
+            err = np.sqrt(eff*(1-eff)/5000.)
+            test = hist.GetBinError(searchbinx,searchbiny)
 		
-		weight *= (1.0 - SF * eff)
-		weight_up *= (1.0 - SFup * (eff + err))
-		weight_down *= (1.0 - SFdown * (eff - err))
+            weight *= (1.0 - SF * eff)
+            weight_up *= (1.0 - SFup * (eff + err))
+            weight_down *= (1.0 - SFdown * (eff - err))
 	  	
         self.out.fillBranch(self.name, weight)
         if self.doSysVar:
-                self.out.fillBranch(self.name+"Up", weight_up)
-                self.out.fillBranch(self.name+"Down", weight_down)
+            self.out.fillBranch(self.name+"Up", weight_up)
+            self.out.fillBranch(self.name+"Down", weight_down)
         return True
 
 # define modules using the syntax 'name = lambda : constructor' to avoid having them loaded when not needed

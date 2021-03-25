4d8dccb56bfd30dd7067cfba001819a686353619
Chad Freer <chad.freer@cern.ch>
2021-03-25 05:56:41 +0100

    changes for SUEP analysis 	deleted:    condor/check_hlt.py 	deleted:    condor/combineHLT_Run2.yaml 	modified:   condor/condor_Run2_proc.py 	deleted:    condor/condor_VBS_proc.py 	deleted:    condor/condor_WSProducer.py 	modified:   condor/keep_and_drop.txt 	deleted:    condor/run_VBS.py 	new file:   python/.__afsD13E 	deleted:    python/ADDProducer.py 	deleted:    python/EWProducer.py 	deleted:    python/MonoZWSProducer.py 	deleted:    python/NvtxPUreweight.py 	new file:   python/SUEPProducer.py 	deleted:    python/VBSProducer.py 	deleted:    python/s_hatProducer.py

--- condor/condor_Run2_proc.py
+++ condor/condor_Run2_proc.py
@@ -1,8 +1,8 @@
 import os, sys, re
 import ROOT
 ROOT.PyConfig.IgnoreCommandLineOptions = True
-# import PSet
 import yaml
+
 #Import the NanoAOD-tools that we will need
 from PhysicsTools.NanoAODTools.postprocessing.framework.postprocessor import PostProcessor
 from PhysicsTools.NanoAODTools.postprocessing.modules.btv.btagSFProducer import *
@@ -14,16 +14,14 @@ from PhysicsTools.NanoAODTools.postprocessing.modules.common.puWeightProducer im
 from PhysicsTools.NanoAODTools.postprocessing.modules.common.muonScaleResProducer import *
 from PhysicsTools.NanoAODTools.postprocessing.modules.common.lepSFProducer import *
 from PhysicsTools.NanoAODTools.postprocessing.modules.common.PrefireCorr import *
+
 #Import the MonoZ analysis tools
-from PhysicsTools.MonoZ.MonoZProducer import *
-from PhysicsTools.MonoZ.GenWeightProducer import *
-from PhysicsTools.MonoZ.EWProducer import *
-from PhysicsTools.MonoZ.ADDProducer import *
-from PhysicsTools.MonoZ.NvtxPUreweight import *
-from PhysicsTools.MonoZ.PhiXYCorrection import *
-from PhysicsTools.MonoZ.BtagEventWeightProducer import *
-from PhysicsTools.MonoZ.TriggerSFProducer import *
-from PhysicsTools.MonoZ.GenMonoZProducer import *
+from PhysicsTools.SUEP.SUEPProducer import *
+from PhysicsTools.SUEP.GenWeightProducer import *
+from PhysicsTools.SUEP.PhiXYCorrection import *
+#from PhysicsTools.SUEP.BtagEventWeightProducer import *
+#from PhysicsTools.SUEP.TriggerSFProducer import *
+#from PhysicsTools.SUEP.GenMonoZProducer import *
 import argparse
 
 parser = argparse.ArgumentParser("")
@@ -78,28 +76,10 @@ print "---------------------------"
 
 xsection = 1.0
 nevents = 1
-if options.isMC:
-   condtag_ = "NANOAODSIM"
-   if options.dataset == "X":
-      options.dataset = options.infile
-      options.dataset = options.dataset.split('/store')[1].split("/")
-      condtag_ = options.dataset[5]
-      options.dataset = options.dataset[3]
-   print "[check] condtag_ == ", condtag_
-   print "[check] dataset  == ", options.dataset
-else:
-   if options.dataset == "X":
-      options.dataset = options.infile
-      options.dataset = options.dataset.split('/store')[1].split("/")
-      condtag_ = options.dataset[2]
-      options.dataset = options.dataset[3]
-   else:
-      options.dataset = options.dataset.split("/")
-      condtag_ = options.dataset[2]
-      options.dataset = options.dataset[1]
+options.dataset = "QCD"
+condtag_ = "SUEP_QCD"
 
-pre_selection  = "((Sum$(Electron_pt>20 & &abs(Electron_eta)<2.5) + Sum$(Muon_pt>20 && abs(Muon_eta)<2.5) )>=2)"
-pre_selection += "&& Flag_METFilters"
+pre_selection = "(HLT_PFHT1050 || HLT_PFJet500)"
 
 if float(options.nevt) > 0:
    print " passing this cut and : ", options.nevt
@@ -108,12 +88,15 @@ if float(options.nevt) > 0:
 modules_era   = [
     GenWeightProducer(
        isMC = options.isMC,
-       dopdf = False if ("ADD" in options.dataset or "Unpart" in options.dataset) else True
+       dopdf =  True
     )
 ]
 
-pro_syst = [ "ElectronEn", "MuonEn", "jesTotal", "jer"]
-ext_syst = [ "puWeight", "PDF", "MuonSF", "ElecronSF", "EWK", "nvtxWeight","TriggerSFWeight","btagEventWeight", "QCDScale0w", "QCDScale1w", "QCDScale2w"]
+#pro_syst = [ "ElectronEn", "MuonEn", "jesTotal", "jer"]
+#ext_syst = [ "puWeight", "PDF", "MuonSF", "ElecronSF", "EWK", "nvtxWeight","TriggerSFWeight","btagEventWeight", "QCDScale0w", "QCDScale1w", "QCDScale2w"]
+pro_syst = []
+ext_syst = []
+
 
 if options.isMC:
    if options.era=="2016":
@@ -123,7 +106,6 @@ if options.isMC:
       modules_era.append(btagSFProducer("Legacy2016", "deepcsv"))
       modules_era.append(muonScaleRes2016())
       modules_era.append(lepSF_2016())
-      modules_era.append(nvtxWeight_2016())
       ext_syst.append("PrefireWeight")
    if options.era=="2017":
       modules_era.append(puAutoWeight_2017())
@@ -132,7 +114,6 @@ if options.isMC:
       modules_era.append(btagSFProducer("2017", "deepcsv"))
       modules_era.append(muonScaleRes2017())
       modules_era.append(lepSF_2017())
-      modules_era.append(nvtxWeight_2017())
       ext_syst.append("PrefireWeight")
    if options.era=="2018":
       modules_era.append(puAutoWeight_2018())
@@ -140,43 +121,21 @@ if options.isMC:
       modules_era.append(btagSFProducer("2018", "deepcsv"))
       modules_era.append(muonScaleRes2018())
       modules_era.append(lepSF_2018())
-      modules_era.append(nvtxWeight_2018())
 
    modules_era.append(PhiXYCorrection(era=options.era,isMC=options.isMC,sys=''))
-   modules_era.append(MonoZProducer(isMC=options.isMC, era=str(options.era), do_syst=1, syst_var=''))
+   modules_era.append(SUEPProducer(isMC=options.isMC, era=str(options.era), do_syst=1, syst_var=''))
 
-   if options.era=="2016":
-      modules_era.append(TriggerSF_2016())
-      modules_era.append(BtagEventWeight_2016())
-   if options.era=="2017":
-      modules_era.append(TriggerSF_2017())
-      modules_era.append(BtagEventWeight_2017())
-   if options.era=="2018":
-      modules_era.append(TriggerSF_2018())
-      modules_era.append(BtagEventWeight_2018())
+   #if options.era=="2016":
+   #   modules_era.append(TriggerSF_2016())
+   #   modules_era.append(BtagEventWeight_2016())
+   #if options.era=="2017":
+   #   modules_era.append(TriggerSF_2017())
+   #   modules_era.append(BtagEventWeight_2017())
+   #if options.era=="2018":
+   #   modules_era.append(TriggerSF_2018())
+   #   modules_era.append(BtagEventWeight_2018())
 
-   modules_era.append(GenMonoZProducer())
-   # WZ or ZZ sample for ewk corrections and ADD for EFT weights
-   if "ZZTo" in options.dataset and "GluGluToContin" not in options.dataset:
-      modules_era.append(EWProducer(1, True))
-   if "WZTo" in options.dataset:
-      modules_era.append(EWProducer(2, False))
-   dim = 0
-   if "ADD" in options.dataset:
-      if "d_2" in options.dataset or "d-2" in options.dataset: dim = 2
-      elif "d_3" in options.dataset or "d-3" in options.dataset: dim = 3
-      elif "d_4" in options.dataset or "d-4" in options.dataset: dim = 4
-      elif "d_5" in options.dataset or "d-5" in options.dataset: dim = 5
-      elif "d_6" in options.dataset or "d-6" in options.dataset: dim = 6
-      elif "d_7" in options.dataset or "d-7" in options.dataset: dim = 7
-      else: sys.exit('ADD model dimensions does not make sense') 
-
-      if "MD_1" in options.dataset or "MD-1" in options.dataset: MD = 1
-      elif "MD_2" in options.dataset or "MD-2" in options.dataset:MD = 2
-      elif "MD_3" in options.dataset or "MD-3" in options.dataset: MD = 3
-      else: MD=100#number large enough the ADDWeight will be 1
-
-      modules_era.append(ADDProducer(MD,dim,options.era))
+   #modules_era.append(GenMonoZProducer())
 
 
    # for shift-based systematics
@@ -184,31 +143,10 @@ if options.isMC:
       for var in ["Up", "Down"]:
 	 if "jesTotal" in sys and options.doSyst==1: modules_era.append(PhiXYCorrection(era=options.era,isMC=options.isMC,sys=sys+var))
 	 if "jer" in sys and options.doSyst==1: modules_era.append(PhiXYCorrection(era=options.era,isMC=options.isMC,sys=sys+var))
-         modules_era.append(MonoZProducer(options.isMC, str(options.era), do_syst=1, syst_var=sys+var))
+         modules_era.append(SUEPProducer(options.isMC, str(options.era), do_syst=1, syst_var=sys+var))
 
 else:
    print "sample : ", options.dataset, " candtag : ", condtag_
-   try:
-      combineHLT = yaml.load(open("combineHLT_Run2.yaml"))
-   except yaml.YAMLError as exc:
-      print(exc)
-   if options.era=="2016":
-      if 'Run2016H' in condtag_:
-	pre_selection = pre_selection + " && (" + combineHLT.get("Run2016H.%s" % options.dataset, "") + ")"
-      else:
-        pre_selection = pre_selection + " && (" + combineHLT.get("Run2016All.%s" % options.dataset, "") + ")"
-   if options.era=="2017":
-      if 'Run2017B' in condtag_:
-         pre_selection = pre_selection + " && (" + combineHLT.get("Run2017B.%s" % options.dataset, "") + ")"
-      elif 'Run2017C' in condtag_:
-         pre_selection = pre_selection + " && (" + combineHLT.get("Run2017C.%s" % options.dataset, "") + ")"
-      else:
-         pre_selection = pre_selection + " && (" + combineHLT.get("Run2017CF.%s" % options.dataset, "") + ")"
-   if options.era=="2018":
-      if ('Run2018A' in condtag_) or ('Run2018B' in condtag_):
-         pre_selection = pre_selection + " && (" + combineHLT.get("Run2018AB.%s" % options.dataset, "") + ")"
-      else:
-         pre_selection = pre_selection + " && (" + combineHLT.get("Run2018CD.%s" % options.dataset, "") + ")"
 
    print " -- era : ",
    if options.era=="2016":
@@ -219,7 +157,7 @@ else:
       modules_era.append(getattr(jetRecalib, 'jetRecalib2018%s' % condtag_.split(options.era)[1][:1])() )
 
    modules_era.append(PhiXYCorrection(era=options.era,isMC=options.isMC,sys=''))
-   modules_era.append(MonoZProducer  (isMC=options.isMC, era=str(options.era), do_syst=1, syst_var=''))
+   modules_era.append(SUEPProducer  (isMC=options.isMC, era=str(options.era), do_syst=1, syst_var=''))
 
    if options.era=="2016":
        options.json = "Cert_271036-284044_13TeV_ReReco_07Aug2017_Collisions16_JSON.txt"

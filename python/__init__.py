ef1a197006cd9af6431b0371a950a28395478911
Chad <chad.freer@cern.ch>
2021-03-30 19:06:49 -0400

    Update __init__.py

--- python/__init__.py
+++ python/__init__.py
@@ -1,3 +1,3 @@
 #Automatically created by SCRAM
 import os
-__path__.append(os.path.dirname(os.path.abspath(__file__).rsplit('/PhysicsTools/MonoZ/',1)[0])+'/cfipython/slc6_amd64_gcc630/PhysicsTools/MonoZ')
+__path__.append(os.path.dirname(os.path.abspath(__file__).rsplit('/PhysicsTools/SUEP/',1)[0])+'/cfipython/slc6_amd64_gcc630/PhysicsTools/SUEP')
